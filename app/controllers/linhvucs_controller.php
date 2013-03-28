4b9006697ae5e29bb475a6d511f5617a9d4816e1
trangnguyenthutrangBK90@gmail.com <trangnguyenthutrangBK90@gmail.com@75745697-65d5-c504-0ae2-17f402973e89>
2013-03-28 07:22:03 +0000

    controller linhvuc

---
+++ app/controllers/linhvucs_controller.php
@@ -0,0 +1,61 @@
+<?php
+class LinhvucsController extends AppController {
+
+	var $name = 'Linhvucs';
+
+	function index() {
+		$this->Linhvuc->recursive = 0;
+		$this->set('linhvucs', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid linhvuc', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('linhvuc', $this->Linhvuc->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->Linhvuc->create();
+			if ($this->Linhvuc->save($this->data)) {
+				$this->Session->setFlash(__('The linhvuc has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The linhvuc could not be saved. Please, try again.', true));
+			}
+		}
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid linhvuc', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Linhvuc->save($this->data)) {
+				$this->Session->setFlash(__('The linhvuc has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The linhvuc could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Linhvuc->read(null, $id);
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for linhvuc', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Linhvuc->delete($id)) {
+			$this->Session->setFlash(__('Linhvuc deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Linhvuc was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+}
