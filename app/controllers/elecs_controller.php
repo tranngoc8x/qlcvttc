62bf87e0838b305a94fd4b06287b19a5311faa9b
trangnguyenthutrangBK90@gmail.com <trangnguyenthutrangBK90@gmail.com@75745697-65d5-c504-0ae2-17f402973e89>
2013-05-07 08:58:43 +0000

    thêm listviewsdate

--- app/controllers/elecs_controller.php
+++ app/controllers/elecs_controller.php
@@ -30,8 +30,7 @@ class ElecsController extends AppController {
 		$this->set(compact('cus'));
 		}
 
-	function listview() {
-		//App::import('Libs', 'libchart/classes/libchart.php');
+	function listview() {		
 		$this->Elec->recursive = -1;
 		if(isset($this->data['Elec']['YM'])){
 			$y = (int)$this->data['Elec']['YM']['year'];
@@ -45,6 +44,68 @@ class ElecsController extends AppController {
 		$this->set(compact('y','cus','m'));
 		//debug($cus);
 	}
+	
+	function listviewsdate(){
+		$this->Elec->recursive = -1;
+		$this->loadModel('Customer');
+		$cus = $this->Customer->find('all');
+		$this->set(compact('cus'));
+		if(isset($this->data)){			
+			$date1 = implode('-',array_reverse(explode('/',$this->data['Elec']['date1'])));
+			$date2 = implode('-',array_reverse(explode('/',$this->data['Elec']['date2'])));			
+			$diff = strtotime($date2)-strtotime($date1);
+			if($diff > 0){
+				$d = $diff/(60*60*24);
+				if($d<=32){
+					$a = explode('/',$this->data['Elec']['date1']);
+					$y1 = $a[2];
+					$m1 = $a[1];
+					$d1 = $a[0];
+					$b = explode('/',$this->data['Elec']['date2']);
+					$y2 = $b[2];
+					$m2 = $b[1];
+					$d2 = $b[0];					
+				}else{
+					$y2 = date('Y');
+					$m2 = date('n');
+					$d2 = date('d');
+					$predate = strtotime('-1 month', strtotime(date('Y-n-d')));
+					$predate = date('Y-n-d',$predate);			
+					$pre = explode('-',$predate);
+					$y1 = $pre[0];
+					$m1 = $pre[1]; 
+					$d1 = $pre[2];
+					$d = (strtotime(date('Y-n-d')) - strtotime($predate))/(60*60*24);
+					$this->Session->setFlash(__('Hãy chọn lại khoảng thời gian. Hệ thống không hiển thị dữ liệu quá 1 tháng ', true));
+				}					
+			}else{
+				$y2 = date('Y');
+				$m2 = date('n');
+				$d2 = date('d');
+				$predate = strtotime('-1 month', strtotime(date('Y-n-d')));
+				$predate = date('Y-n-d',$predate);			
+				$pre = explode('-',$predate);
+				$y1 = $pre[0];
+				$m1 = $pre[1]; 
+				$d1 = $pre[2];
+				$d = (strtotime(date('Y-n-d')) - strtotime($predate))/(60*60*24);
+				$this->Session->setFlash(__('Hãy chọn lại khoảng thời gian. Ngày bắt đầu không thể nhỏ hơn ngày kết thúc ', true));
+			}
+		}else {
+			$y2 = date('Y');
+			$m2 = date('n');
+			$d2 = date('d');
+			$predate = strtotime('-1 month', strtotime(date('Y-n-d')));
+			$predate = date('Y-n-d',$predate);			
+			$pre = explode('-',$predate);
+			$y1 = $pre[0];
+			$m1 = $pre[1]; 
+			$d1 = $pre[2];
+			$d = (strtotime(date('Y-n-d')) - strtotime($predate))/(60*60*24);
+		}
+		$this->set(compact('y1','m1','d1','y2','m2','d2','d'));
+	}
+
 
 	function add() {
 		$this->loadModel("Customer");
@@ -53,23 +114,28 @@ class ElecsController extends AppController {
 		$this->loadModel("Room");
 		$room = $this->Room->find('count');
 
-		if (!empty($this->data)) {
-		//debug($this->data['Elec']['date']);
+		if (!empty($this->data)) {		
 			if(!empty($this->data['Elec']['date'])){
 				$dstart = explode('/',$this->data['Elec']['date']);
 				$ds = $dstart[2].'-'.$dstart[1].'-'.$dstart[0] ;
 				$this->data['Elec']['date'] = date('Y-m-d',strtotime($ds));
+				//$first = date('Y-m-d',strtotime($ds.'-1day'));
 			}
+			//debug($this->data['Elec']['date']);
+			//debug($first);
 			$j=0;
 			$data= array();
 			$a = array();
 			for($i=1;$i<=$room;$i++){
 				$this->Elec->create();
 				if(!empty($this->data['Elec']['elec'.$i])){
+					
 					$data['Elec']['rooms_id']=$this->data['Elec']['rooms_id'.$i];
 					$data['Elec']['elec']=$this->data['Elec']['elec'.$i];
 					$data['Elec']['date']=$this->data['Elec']['date'];
+					//$elec_first = $this->Elec->find("first",array('fields'=>array('elec'),'conditions'=>array('rooms_id'=>$this->data['Elec']['rooms_id'.$i],'date'=>$first)));
 					// Kiem tra xem ngay do da nhap du lieu chua, neu nhap roi thi sua con chua thi them moi
+					//$data['Elec']['mtt'] = $this->data['Elec']['elec'.$i] - $elec_first;
 					$arr = $this->Elec->find('all', array('conditions'=>array('date'=>$this->data['Elec']['date'], 'rooms_id'=>$data['Elec']['rooms_id'])));
 					if(empty($arr)){
 						$this->Elec->save($data);
@@ -78,6 +144,7 @@ class ElecsController extends AppController {
 					}else{
 						$this->Elec->id = $arr[0]['Elec']['id'];
 						$this->Elec->saveField('elec',$data['Elec']['elec']);
+						//$this->Elec->saveField('mtt',$data['Elec']['elec']);
 						}
 				}
 			}
@@ -88,7 +155,7 @@ class ElecsController extends AppController {
 					$a = $room- $j;
 					$this->Session->setFlash(__('Đã thêm mới '.$j.' phòng. Và sửa '.$a.' phòng.' , true));
 				}else
-					$this->Session->setFlash(__('Đã sửa' , true));
+					$this->Session->setFlash(__('Đã lưu' , true));
 
 			$this->redirect(array('action' => 'listview'));
 		}
